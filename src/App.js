0a46b5cb31efc36c6bea6761a58cf2c6e5474ec5
wawazer <wawazer@gmail.com>
2023-07-31 15:39:10 +0700

    create env

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>test</h1>
+        <div className="Movie-container">
+          <div className="movie-wrapper">
+            <div className="movie-title">
+              <img className="movie-image" />
+              <div className="movie-date"></div>
+              <div className="movie-rate"></div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
